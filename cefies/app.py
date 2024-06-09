52664253bd9abbde857e768757ab56c5965acbff
Ren <renrror@gmail.com>
2024-06-09 20:00:11 +0700

    fix: add root path

--- cefies/app.py
+++ cefies/app.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI
 import cefies.internal.firestore  # noqa: F401 -- Intended to initialize Firebase
 from cefies.routes import index_router, auth_router
 
-app = FastAPI()
+app = FastAPI(root_path="/api")
 app.include_router(index_router)
 app.include_router(auth_router)
